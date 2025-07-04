a5534006a5dbe0f08c365e68b6bc665fc1eaa004
Rakesh <dr8452799@gmail.com>
2025-07-04 21:21:21 +0530

    Update Strong Number or not.java

--- company/Strong Number or not.java
+++ company/Strong Number or not.java
@@ -1,6 +1,6 @@
 package com.imp;
 import java.util.*;
-/*
+/* This Question Asked in the Written Test at CloudOdsessy Company [Manyata Tech Park ] 
  * Write a Java program to check whether a given number is a Strong Number or not.
 (A Strong Number is a number whose sum of the factorials 
 of its digits is equal to the number itself. Example: 145 → 1! + 4! + 5! = 145)
